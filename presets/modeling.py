9dbee78d68ec7e3f09c702084781f158453abf3e
dg <dg>
2020-09-16 07:57:56 +0200

    updated presets

--- presets/modeling.py
+++ presets/modeling.py
@@ -1,22 +1,34 @@
 import bpy
-buttons = bpy.context.scene.shelf_list
+shelf = bpy.context.scene.shelf_list
 
-buttons.clear()
-buttons_sub_1 = buttons.add()
-buttons_sub_1.name = ''
-buttons_sub_1.button_name = 'suzanne'
-buttons_sub_1.button_operator = 'mesh.primitive_monkey_add'
-buttons_sub_1.button_icon = 'MONKEY'
-buttons_sub_1.show_button_name = False
-buttons_sub_1 = buttons.add()
-buttons_sub_1.name = ''
-buttons_sub_1.button_name = 'cube'
-buttons_sub_1.button_operator = 'mesh.primitive_cube_add'
-buttons_sub_1.button_icon = 'MESH_CUBE'
-buttons_sub_1.show_button_name = False
-buttons_sub_1 = buttons.add()
-buttons_sub_1.name = ''
-buttons_sub_1.button_name = 'sphere'
-buttons_sub_1.button_operator = 'mesh.primitive_ico_sphere_add'
-buttons_sub_1.button_icon = 'MESH_ICOSPHERE'
-buttons_sub_1.show_button_name = False
+shelf.clear()
+item_sub_1 = shelf.add()
+item_sub_1.name = ''
+item_sub_1.button_name = 'suzanne'
+item_sub_1.button_operator = 'mesh.primitive_monkey_add'
+item_sub_1.button_icon = 'MONKEY'
+item_sub_1.show_button_name = False
+item_sub_1 = shelf.add()
+item_sub_1.name = ''
+item_sub_1.button_name = 'cube'
+item_sub_1.button_operator = 'mesh.primitive_cube_add'
+item_sub_1.button_icon = 'MESH_CUBE'
+item_sub_1.show_button_name = False
+item_sub_1 = shelf.add()
+item_sub_1.name = ''
+item_sub_1.button_name = 'sphere'
+item_sub_1.button_operator = 'mesh.primitive_ico_sphere_add'
+item_sub_1.button_icon = 'MESH_ICOSPHERE'
+item_sub_1.show_button_name = False
+item_sub_1 = shelf.add()
+item_sub_1.name = ''
+item_sub_1.button_name = 'Import FBX'
+item_sub_1.button_operator = 'import_scene.fbx'
+item_sub_1.button_icon = 'IMPORT'
+item_sub_1.show_button_name = False
+item_sub_1 = shelf.add()
+item_sub_1.name = ''
+item_sub_1.button_name = 'Export FBX'
+item_sub_1.button_operator = 'export_scene.fbx'
+item_sub_1.button_icon = 'EXPORT'
+item_sub_1.show_button_name = False
